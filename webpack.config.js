d8287c6f2c1441d655b3f35468826cb8bc92aeca
Basil <basil@icloud.com>
2021-07-18 14:47:50 +0200

    edits

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,15 @@ module.exports = {
     module: {
     rules: [
     {
-    test: /\.(js|jsx)$/,
-    exclude: /node_modules/,
-    use: {
-    loader:'babel-loader',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+        loader:'babel-loader',
+        },
     },
+    {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
     },
     ],
     },
